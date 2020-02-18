0c07ac0b3e846ea787214e8f7e815567d1c8f3a4
fangyuexi <fangyuexifyx@outlook.com>
2020-02-18 13:01:10 +0000

    From Ruby

--- PICU/src/app/app-routing.module.ts
+++ PICU/src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'faq',
+    loadChildren: () => import('./pages/faq/faq.module').then( m => m.FaqPageModule)
   }
 ];
 @NgModule({
---
+++ PICU/src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,78 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../home/home.module').then(m => m.HomePageModule)
+          }
+        ]
+      },
+      {
+        path: 'picu',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../picu/picu.module').then(m => m.PicuPageModule)
+          }
+        ]
+      },
+      {
+        path: 'yourchild',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../yourchild/yourchild.module').then(m => m.YourchildPageModule)
+          }
+        ]
+      },
+      {
+        path: 'team',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../team/team.module').then(m => m.TeamPageModule)
+          }
+        ]
+      },
+      {
+        path: 'visit',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../visit/visit.module').then(m => m.VisitPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
