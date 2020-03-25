abe2745fb8b1b2f0a3b2cdcae46b4d598f7fc618
Kamile Sakelyte <kamile.sakelyte.19@ucl.ac.uk>
2020-03-25 18:58:42 +0000

    pictures displayed as slides

--- PICU/src/app/pages/team-detail/team-detail.page.ts
+++ PICU/src/app/pages/team-detail/team-detail.page.ts
@@ -14,13 +14,15 @@ export class TeamDetailPage implements OnInit {
     this.isDisabled = true;
   }
 
-  public contents : Array<{image: string, text: string, parent: number}>;
+  public contents : Array<{image1: string, image2: string, image3: string, text: string, parent: number}>;
 
   // This bit should take the information from the DB query results that is relevant to a specific page (using page ID)
 
   constructor(private _router : Router) { 
     this.contents =[
-      { image : '../assets/Team_Pic.jpg',
+      { image1 : '../assets/yourchild_photos/bedspace.png',
+        image2 : '../assets/yourchild_photos/ventilator1.png',
+        image3 : null,
         text : 'Text with relevant contents',
         parent : 1
       }
