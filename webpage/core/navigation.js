1b44b1e9bb1962562e10d2bec0805aaf626f7333
Kamile Sakelyte <kamile.sakelyte.19@ucl.ac.uk>
2020-04-16 13:22:59 +0100

    navigation

---
+++ webpage/core/navigation.js
@@ -0,0 +1,13 @@
+const pool = require('./pool');
+
+class PicuNavigation {
+    find(callback) {
+        let sql = 'SELECT Nav_name FROM navigation WHERE Page_id = "2"';
+        pool.query(sql, function(err, result) {
+            if(err) return console.log(err)
+            if(result) return result
+        })
+    };
+};
+
+module.exports = PicuNavigation;
--- webpage/routes/pages.js
+++ webpage/routes/pages.js
@@ -4,12 +4,21 @@ const User = require('../core/user');
 
 const user = new User();
 
+const PicuNavigation = require('../core/navigation');
+const picuNavigation = new PicuNavigation();
+const picuNav = picuNavigation.find();
+
 //login page
 router.get('/login', (req, res, next) => res.render('login.ejs'));
 //home page
-router.get('/home', (req, res, next) => res.render('home.ejs'));
+router.get('/home', (req, res, next) => res.send('home.ejs', {picuNav}));
 //picu page
 router.get('/picu', (req, res, next) => res.render('picu.ejs'));
+
+//sidebar navigation
+//router.get('/', (req, res, next) => {
+//    res.render('partials/header.ejs', {picuNav: picuNavigation.find()})
+//});
 
 //post login data
 router.post('/login', (req, res, next) => {
